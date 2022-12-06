652fe521eaf8158e05b9b0538652e3171d761b80
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-06 09:55:08 +0530

    Create Acronym.py

---
+++ Acronym.py
@@ -0,0 +1,11 @@
+text = str(input("Enter a String :"))
+text = text.split()
+acronym = ""
+for i in text :
+  acronym = acronym+str(i[0]).upper()
+  print(acronym)
+  
+# Below is the Output :
+
+Python Programming Language
+PPL
