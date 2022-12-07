cf856c1f7fb815e8e1810781035899a8653b524a
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-07 09:45:24 +0530

    Create Tables.py

---
+++ Tables.py
@@ -0,0 +1,19 @@
+num = int(input("Display multiplication table of? "))
+
+for i in range(1, 11):            # Iterate 10 times from i = 1 to 10                    
+    print(num, 'x', i, '=', num*i)
+
+# Below is the Output :
+
+Display multiplication table of? 5
+
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
