d705d00dcb424d817031a2c4ff75a343ec702b4c
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-08 08:59:23 +0530

    Create Swap.py

---
+++ Swap.py
@@ -0,0 +1,17 @@
+x = 8
+y = 71
+
+x = input('Enter value of x: ')         # To take inputs from the user
+y = input('Enter value of y: ')
+
+temp = x                                 # create a temporary variable and swap the values  
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
+#  Below is the Output :
+
+The value of x after swapping: 71
+The value of y after swapping: 8
