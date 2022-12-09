ae54214b8c15b345ec5bcda749ee1eae67f96f5a
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-09 09:34:35 +0530

    Create Deci_Octa_Hexa.py

---
+++ Deci_Octa_Hexa.py
@@ -0,0 +1,13 @@
+dec = 513
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
+# Below is the Output :
+
+The decimal value of 513 is:
+0b1000000001 in binary.
+0o1001 in octal.
+0x201 in hexadecimal.
