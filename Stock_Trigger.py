10dde214ec13629b586fb23c68730d7c31f1c413
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-11 22:49:31 +0530

    Create Stock_Trigger.py

---
+++ Stock_Trigger.py
@@ -0,0 +1,11 @@
+stock_price_ABC = 300
+
+if (stock_price_ABC > 250):                         # if condition 1 is true then
+    print("I will sell the stock and book profit")
+
+else:
+    print("I will keep buying the stock")
+    
+# Below is the Output :
+
+I will sell the stock and book profit
