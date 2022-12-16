a5187479fe81074d0f4049753a9ba34b93c29f50
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-16 10:18:13 +0530

    Create Dict_Update.py

---
+++ Dict_Update.py
@@ -0,0 +1,9 @@
+mydict = {'StuName': 'Ram', 'StuAge': 5, 'StuCity': 'Mumbai'}
+mydict['StuClass'] = 'Sr.KG'
+print("Student Name is:", mydict['StuName'])
+print("Student Class is:", mydict['StuClass'])
+
+# Below is the Output :
+
+Student Name is : Ram
+Student Class is : Sr.KG
